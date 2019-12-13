773242c05dafe8e08fd22814f9d5a1df9e065382
soniacb88 <soniacaceres36@gmail.com>
2019-12-13 12:56:16 +0100

    ok

--- project-management-client/src/components/map-component/mapComponent.jsx
+++ project-management-client/src/components/map-component/mapComponent.jsx
@@ -18,10 +18,10 @@ const API_CONFIG = {
 export default class CustomMap extends Component {
     componentWillUnmount() {
         const allScripts = document.getElementsByTagName('script');
-        [].filter.call(
-            allScripts,
-            (scpt) => scpt.src.indexOf(`key=${process.env.REACT_APP_GOOGLE_KEY}`) >= 0
-        )[0].remove();
+        // [].filter.call(
+        //     allScripts,
+        //     (scpt) => scpt.src.indexOf(`key=${process.env.REACT_APP_GOOGLE_KEY}`) >= 0
+        //)[0].remove();
         window.google = {};
     }
     componentDidMount() {
