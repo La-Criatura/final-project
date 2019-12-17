cf670df3cb0700d1ed6a1750454a67369bf56b9e
adelcanto <a.delcanto@outlook.com>
2019-12-17 11:50:06 +0100

    changing favicon

--- react-day4-server-starter/app.js
+++ react-day4-server-starter/app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
+
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
@@ -49,7 +49,7 @@ app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
 
 // ADD SESSION SETTINGS HERE:
 
