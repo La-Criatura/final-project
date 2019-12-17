6ed2823e1e534957359a26bbe8519c9013939c2a
adelcanto <a.delcanto@outlook.com>
2019-12-17 10:51:27 +0100

    before heroku deployment

--- react-day4-server-starter/app.js
+++ react-day4-server-starter/app.js
@@ -92,4 +92,6 @@ app.use('/api', require('./routes/skill-routes'));
 app.use('/api', require('./routes/auth-routes'))
 // app.use('/api', require('./routes/file-upload-routes'));
 
+app.use((req, res) => {     res.sendFile(__dirname + "/public/index.html");    });
+
 module.exports = app;
---
+++ react-day4-server-starter/public/precache-manifest.4e5df48bea5c204f211ebbdebdab2336.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4379aad6a6886409eab196ae26bc65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a080112129bb909c4dde",
+    "url": "/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "97985b23e7c0d49c5464",
+    "url": "/static/css/main.cb7e836c.chunk.css"
+  },
+  {
+    "revision": "a080112129bb909c4dde",
+    "url": "/static/js/2.9bcfd3c3.chunk.js"
+  },
+  {
+    "revision": "4b36eb37fa1d44c7bd0d9c5c3ce6c7e9",
+    "url": "/static/js/2.9bcfd3c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "97985b23e7c0d49c5464",
+    "url": "/static/js/main.a175b5a1.chunk.js"
+  },
+  {
+    "revision": "d168a37d23f6613f153a",
+    "url": "/static/js/runtime-main.124e7173.js"
+  }
+]);
